2033e5735b00bd887a4ba357af1e3651f0c9d5e6
bronek14 <116382641+bronek14@users.noreply.github.com>
2023-09-18 19:09:23 -0700

    Made corrections to arguments

--- redditScraper.py
+++ redditScraper.py
@@ -2,24 +2,24 @@ import argparse
 import requests
 from bs4 import BeautifulSoup
 
-def download_text_content(url, data_file):
+def download_text_content(url, output_file):
     try:
         # Send an HTTP GET request to the URL
         response = requests.get(url)
 
         # Check if the request was successful (status code 200)
         if response.status_code == 200:
-            # Parse the HTML content  using BeautifulSoup
+            # Parse the HTML content of the page using BeautifulSoup
             soup = BeautifulSoup(response.text, 'html.parser')
 
             # Extract text data from the page
             text_data = soup.get_text()
 
             # Write the text data to the output file
-            with open(data_file, 'w', encoding='utf-8') as file:
+            with open(output_file, 'w', encoding='utf-8') as file:
                 file.write(text_data)
 
-            print(f"Text content downloaded and saved to {data_file}")
+            print(f"Text content downloaded and saved to {output_file}")
         else:
             print(f"Failed to retrieve content. Status code: {response.status_code}")
 
@@ -32,7 +32,7 @@ def main():
     parser.add_argument("output_file", help="The name of the output text file")
 
     args = parser.parse_args()
-    download_text_content(args.URL, args.data_file)
+    download_text_content(args.URL, args.output_file)
 
 if __name__ == "__main__":
     main()
