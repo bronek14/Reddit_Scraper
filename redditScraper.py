18a9b644d9aa9f17346b9c53785132f4ccce6064
bronek14 <116382641+bronek14@users.noreply.github.com>
2023-10-02 12:56:30 -0700

    Update redditScraper.py

--- redditScraper.py
+++ redditScraper.py
@@ -1,31 +1,39 @@
 import requests
 from bs4 import BeautifulSoup
 
-# Function to scrape a Reddit thread and save it to a text file
-def scrape_reddit_thread(url, output_file):
+# Function to scrape Reddit data from a URL and save it to a file
+def scrape_reddit_data(url, output_file):
     try:
-        # Send a GET request to the Reddit thread URL
+        # Send a GET request to the Reddit URL
         response = requests.get(url)
 
         # Check if the request was successful (status code 200)
         if response.status_code == 200:
+            # Parse the HTML content of the page
             soup = BeautifulSoup(response.text, 'html.parser')
 
-            # Find all the comment elements
-            comments = soup.find_all(class_='comment')
+            # Find and extract the relevant data (e.g., post titles, comments)
+            # Modify this part to extract the specific data you're interested in
+            data_to_extract = []
 
-            # Open the output file for writing
+            # Save the extracted data to the output file
             with open(output_file, 'w', encoding='utf-8') as file:
-                for comment in comments:
-                    # Extract and write the text of each comment to the file
-                    comment_text = comment.find(class_='md').get_text()
-                    file.write(comment_text)
-                    file.write('\n\n')
+                for item in data_to_extract:
+                    file.write(item + '\n')
 
-            print(f"Scraped data from {len(comments)} comments and saved to {output_file}")
+            print(f"Data scraped and saved to {output_file}")
         else:
-            print(f"Failed to fetch data from {url}. Status code: {response.status_code}")
+            print(f"Failed to retrieve data from {url}. Status code: {response.status_code}")
 
     except Exception as e:
         print(f"An error occurred: {str(e)}")
 
+if __name__ == "__main__":
+    # Input Reddit URL
+    reddit_url = input("Enter the Reddit URL: ")
+
+    # Output file name
+    output_file = "output.txt"
+
+    # Call the function to scrape and save data
+    scrape_reddit_data(reddit_url, output_file)
