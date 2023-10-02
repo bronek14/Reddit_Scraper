15b24453237af36d8f2df34752b95d005a7e0377
bronek14 <116382641+bronek14@users.noreply.github.com>
2023-10-02 13:15:34 -0700

    Update redditScraper.py

--- redditScraper.py
+++ redditScraper.py
@@ -1,44 +1,37 @@
 import requests
 from bs4 import BeautifulSoup
-import nltk
-from nltk.tokenize import sent_tokenize
-import re
-import os
-nltk.download('punkt')
 
-# Prompt the user for the Reddit URL
-reddit_url = input("Enter the Reddit URL you want to scrape: ")
+# Function to scrape and save data
+def scrape_and_save(url, output_filename):
+    try:
+        # Send a GET request to the provided URL
+        response = requests.get(url)
 
-# Make an HTTP GET request to the URL
-response = requests.get(reddit_url)
+        # Check if the request was successful (status code 200)
+        if response.status_code == 200:
+            # Parse the HTML content of the page using BeautifulSoup
+            soup = BeautifulSoup(response.text, 'html.parser')
 
-if response.status_code == 200:
-    # Parse the HTML content of the page using BeautifulSoup
-    soup = BeautifulSoup(response.text, 'html.parser')
-    
-    # Find and extract all user comments
-    comments = soup.find_all("div", {"class": "md"})
+            # Find all text content within <p> and <h1> tags, you can modify this as needed
+            lines = [tag.text for tag in soup.find_all(['p', 'h1'])]
 
-    # Create a directory to store user-specific files
-    output_directory = 'reddit_comments'
-    if not os.path.exists(output_directory):
-        os.makedirs(output_directory)
+            # Combine the lines into a single string
+            data = '\n'.join(lines)
 
-    for comment in comments:
-        # Extract the username and comment text
-        user_tag = comment.find_previous("a", {"class": "author"})
-        if user_tag:
-            username = user_tag.text
-            comment_text = comment.get_text()
-            
-            # Remove extra spaces and line breaks from the comment text
-            comment_text = re.sub(r'\s+', ' ', comment_text).strip()
+            # Save the data to the output file
+            with open(output_filename, 'w', encoding='utf-8') as file:
+                file.write(data)
 
-            # Create a file for each user and write their comments
-            user_file_path = os.path.join(output_directory, f"{username}.txt")
-            with open(user_file_path, 'a', encoding='utf-8') as user_file:
-                user_file.write(comment_text + '\n')
-    
-    print(f"Data scraped and saved in the '{output_directory}' directory.")
-else:
-    print(f"Failed to fetch the Reddit page. Status code: {response.status_code}")
+            print(f'Data scraped successfully and saved to {output_filename}')
+        else:
+            print(f'Error: Unable to fetch data from the URL. Status code: {response.status_code}')
+
+    except Exception as e:
+        print(f'An error occurred: {str(e)}')
+
+# Input URL and output file name
+url = input('Enter the URL you want to scrape: ')
+output_filename = 'output.txt'
+
+# Call the scrape_and_save function
+scrape_and_save(url, output_filename)
