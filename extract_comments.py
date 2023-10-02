d6c1884a3bb335f4e0dceda09927c52975f3ddf8
bronek14 <116382641+bronek14@users.noreply.github.com>
2023-10-02 13:31:31 -0700

    Update extract_comments.py

--- extract_comments.py
+++ extract_comments.py
@@ -5,28 +5,28 @@ def extract_contents(input_filename, output_filename):
             lines = input_file.readlines()
 
         # Create a list to store the extracted contents
-        comments = []
+        extracted_contents = []
 
         # Flag to indicate when to start/stop extracting contents
-        comments = False
+        extracting_contents = False
 
         for line in lines:
             # Check if the line starts with "[-]" to start extracting contents
             if line.strip().startswith("[-]"):
-                if comments:
+                if extracting_contents:
                     # Stop extracting if we were already extracting
-                    comments = False
+                    extracting_contents = False
                 else:
                     # Start extracting if not already extracting
-                    comments = True
+                    extracting_contents = True
                 continue
             # Check if we are currently extracting contents and the line is not empty
-            if comments and line.strip():
-                comments.append(line.strip())
+            if extracting_contents and line.strip():
+                extracted_contents.append(line.strip())
 
         # Save the extracted contents to output.txt
         with open(output_filename, 'w', encoding='utf-8') as output_file:
-            output_file.write('\n'.join(comments))
+            output_file.write('\n'.join(extracted_contents))
 
         print(f'Contents extracted and saved to {output_filename}')
     except Exception as e:
@@ -34,7 +34,7 @@ def extract_contents(input_filename, output_filename):
 
 # Input and output file names
 input_filename = 'output.txt'
-output_filename = 'comments.txt'
+output_filename = 'extracted_contents.txt'
 
 # Call the extract_contents function
-comments(input_filename, output_filename)
+extract_contents(input_filename, output_filename)
