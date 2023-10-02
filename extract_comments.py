330a4806e3dcdcf0ae1281205653c6e469b108ec
bronek14 <116382641+bronek14@users.noreply.github.com>
2023-10-02 13:36:22 -0700

    Update extract_comments.py

--- extract_comments.py
+++ extract_comments.py
@@ -1,40 +1,28 @@
-# Function to extract contents between lines starting with "[-]"
-def extract_contents(input_filename, output_filename):
+# Function to filter and save non-matching lines to comments.txt
+def filter_and_save_comments(input_filename, output_filename):
     try:
         with open(input_filename, 'r', encoding='utf-8') as input_file:
             lines = input_file.readlines()
 
-        # Create a list to store the extracted contents
-        extracted_contents = []
-
-        # Flag to indicate when to start/stop extracting contents
-        extracting_contents = False
+        # Create a list to store the lines that don't start with "[–]"
+        comments = []
 
         for line in lines:
-            # Check if the line starts with "[-]" to start extracting contents
-            if line.strip().startswith("[-]"):
-                if extracting_contents:
-                    # Stop extracting if we were already extracting
-                    extracting_contents = False
-                else:
-                    # Start extracting if not already extracting
-                    extracting_contents = True
-                continue
-            # Check if we are currently extracting contents and the line is not empty
-            if extracting_contents and line.strip():
-                extracted_contents.append(line.strip())
+            # Check if the line doesn't start with "[–]" (you can adjust the text as needed)
+            if not line.strip().startswith("[–]"):
+                comments.append(line.strip())
 
-        # Save the extracted contents to output.txt
+        # Save the filtered lines to comments.txt
         with open(output_filename, 'w', encoding='utf-8') as output_file:
-            output_file.write('\n'.join(extracted_contents))
+            output_file.write('\n'.join(comments))
 
-        print(f'Contents extracted and saved to {output_filename}')
+        print(f'Non-matching lines saved to {output_filename}')
     except Exception as e:
         print(f'An error occurred: {str(e)}')
 
 # Input and output file names
 input_filename = 'output.txt'
-output_filename = 'extracted_contents.txt'
+output_filename = 'comments.txt'
 
-# Call the extract_contents function
-extract_contents(input_filename, output_filename)
+# Call the filter_and_save_comments function
+filter_and_save_comments(input_filename, output_filename)
