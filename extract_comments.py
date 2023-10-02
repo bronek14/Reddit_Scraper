3224630e4f554e61321bb61dbf13ded057fedb3a
bronek14 <116382641+bronek14@users.noreply.github.com>
2023-10-02 13:20:01 -0700

    Update extract_comments.py

--- extract_comments.py
+++ extract_comments.py
@@ -1,22 +1,35 @@
-def extract_comments(input_file="output.txt", output_file="comments.txt"):
+# Function to extract comments and save them to comments.txt
+def extract_comments(input_filename, output_filename):
     try:
-        # Read the content from the input file
-        with open(input_file, "r", encoding="utf-8") as file:
-            content = file.read()
+        with open(input_filename, 'r', encoding='utf-8') as input_file:
+            lines = input_file.readlines()
 
-        # Split the content into separate comments
-        comments = content.split('\n\n')
+        # Create a list to store the extracted comments
+        comments = []
 
-        # Filter out any empty comments
-        comments = [comment.strip() for comment in comments if comment.strip()]
+        # Flag to indicate when to start/stop collecting comments
+        collecting_comments = False
 
-        # Combine the comments into a single string
-        comments_text = "\n\n".join(comments)
+        for line in lines:
+            # Check if the line starts with "Comment:" to start collecting comments
+            if line.strip().startswith("Comment:"):
+                collecting_comments = True
+                continue
+            # Check if we are currently collecting comments and the line is not empty
+            if collecting_comments and line.strip():
+                comments.append(line.strip())
 
-        # Save the comments to the output file
-        with open(output_file, "w", encoding="utf-8") as file:
-            file.write(comments_text)
+        # Save the extracted comments to comments.txt
+        with open(output_filename, 'w', encoding='utf-8') as output_file:
+            output_file.write('\n'.join(comments))
 
-        print(f"Extracted comments successfully and saved to {output_file}")
+        print(f'Comments extracted and saved to {output_filename}')
     except Exception as e:
-        print(f"An error occurred: {str(e)}")
+        print(f'An error occurred: {str(e)}')
+
+# Input and output file names
+input_filename = 'output.txt'
+output_filename = 'comments.txt'
+
+# Call the extract_comments function
+extract_comments(input_filename, output_filename)
