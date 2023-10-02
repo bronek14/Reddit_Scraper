32e4bfe8df64877f6712d4a99075fd4eced36b45
bronek14 <116382641+bronek14@users.noreply.github.com>
2023-10-02 13:30:15 -0700

    Update extract_comments.py

--- extract_comments.py
+++ extract_comments.py
@@ -1,29 +1,34 @@
-# Function to extract comments and save them to comments.txt
-def extract_comments(input_filename, output_filename):
+# Function to extract contents between lines starting with "[-]"
+def extract_contents(input_filename, output_filename):
     try:
         with open(input_filename, 'r', encoding='utf-8') as input_file:
             lines = input_file.readlines()
 
-        # Create a list to store the extracted comments
+        # Create a list to store the extracted contents
         comments = []
 
-        # Flag to indicate when to start/stop collecting comments
-        collecting_comments = False
+        # Flag to indicate when to start/stop extracting contents
+        comments = False
 
         for line in lines:
-            # Check if the line contains "[-]" to start collecting comments
-            if "[-]" in line:
-                collecting_comments = True
+            # Check if the line starts with "[-]" to start extracting contents
+            if line.strip().startswith("[-]"):
+                if comments:
+                    # Stop extracting if we were already extracting
+                    comments = False
+                else:
+                    # Start extracting if not already extracting
+                    comments = True
                 continue
-            # Check if we are currently collecting comments and the line is not empty
-            if collecting_comments and line.strip():
+            # Check if we are currently extracting contents and the line is not empty
+            if comments and line.strip():
                 comments.append(line.strip())
 
-        # Save the extracted comments to comments.txt
+        # Save the extracted contents to output.txt
         with open(output_filename, 'w', encoding='utf-8') as output_file:
             output_file.write('\n'.join(comments))
 
-        print(f'Comments extracted and saved to {output_filename}')
+        print(f'Contents extracted and saved to {output_filename}')
     except Exception as e:
         print(f'An error occurred: {str(e)}')
 
@@ -31,5 +36,5 @@ def extract_comments(input_filename, output_filename):
 input_filename = 'output.txt'
 output_filename = 'comments.txt'
 
-# Call the extract_comments function
-extract_comments(input_filename, output_filename)
+# Call the extract_contents function
+comments(input_filename, output_filename)
